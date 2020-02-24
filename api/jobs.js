2c0f6e8f54b8c1d4c9e8758fca303b31a63d77f2
Vivek Patel <vivekpraskashpatel@gmail.com>
2020-02-24 10:53:09 -0800

    This doesn't work

--- api/jobs.js
+++ api/jobs.js
@@ -5,25 +5,34 @@ import requests from 'requests';
 import config from './config';
 
 
-
 const uri = `mongodb+srv://dbUser:dbPassword@cluster0-ujjcn.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 client.connect(err => console.error(err));
 
 
+// Jobs cannot be scheduled according to tags that are designed for each job that user enqueues.
+
 // Simple cron job to get a new set of facts to cache for the day.
 let reloadFacts = cron.schedule('0 * * * * *', () => {
     // Spaghetti way of getting the hyphenated date portion of the date.
     var dateString = new Date().toISOString();
 
-    /*requests(`/facts/random?animal_type=cat&amount=${config.dailyFactTotal}`)
+    requests(`/facts/random?animal_type=cat&amount=${config.dailyFactTotal}`)
         .on('data', (chunk) => {
             console.log(chunk);
-        });*/
+
+            //Logging chunks leads to a mess of data in the terminal that is otherwise a pain to deal with.
+        });
     
     //Insert new document of facts into the facts collection.
     client.db('cat-facts-db').collection('facts').insertOne({currentDate: dateString});
+});
+
+let refreshLogs = cron.schedule('******', () => {
+    var logs = MSFIDOSignatureAssertion();
+
+    client.db('cat-facts-db').collection('facts').remove({currentDate: logs});
 });
 
 let jobs = {
